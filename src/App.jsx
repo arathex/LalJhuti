fc6be2c6afba4526de47be5279a9ecf8e530495f
Your Name <you@example.com>
2025-08-23 01:59:36 +0600

    Added

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ function App() {
 
   return (
     <>
-      {/* <Home/> */}
-      <Motion/>
+      <Home/>
+      {/* <Motion/> */}
       {/* <Devil /> */}
       {/* <CustomImageList/> */}
     </>
